1763969fcf7f8df948a17b627d11d6bfe18f0926
mohamad <msiysinyuy@gmail.com>
2021-08-20 11:21:11 +0100

    the side bare component created

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import NavBar from "./component/NavBar"
 import MainSection from './component/MainSection'
+import SideBar from "./component/SideBar"
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    <SideBar />
     <MainSection />
 
     </div>
