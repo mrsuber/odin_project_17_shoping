fa7482e949eb8d76e5a27b51a23c580e05dbd9b5
mohamad <msiysinyuy@gmail.com>
2021-08-20 11:06:06 +0100

    the mainsection added

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import NavBar from "./component/NavBar"
+import MainSection from './component/MainSection'
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    <MainSection />
 
     </div>
   );
---
+++ shopping-cart/src/component/MainSection.js
@@ -0,0 +1,9 @@
+import '../styles/MainSection.css'
+
+
+function MainSection(){
+  return(
+    <div className="home__container">this is main section</div>
+  )
+}
+export default MainSection
