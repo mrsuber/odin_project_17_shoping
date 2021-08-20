eec71ce7409ba84eb15812e7bdfe20a5d54c4c32
mohamad <40745841+mrsuber@users.noreply.github.com>
2021-08-20 12:11:43 +0100

    2 home main (#9)
    * the mainsection added
    
    * the home product edited and set.

--- shopping-cart/src/component/MainSection.js
+++ shopping-cart/src/component/MainSection.js
@@ -3,7 +3,18 @@ import '../styles/MainSection.css'
 
 function MainSection(){
   return(
-    <div className="home__container">this is main section</div>
+    <main className="main">
+
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+      <li>Product 4</li>
+      <li>Product 5</li>
+
+    </ul>
+
+    </main>
   )
 }
 export default MainSection
