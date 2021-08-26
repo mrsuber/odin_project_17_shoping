6872f70e589225a8377a0df887b5513f7cd1ee87
mohamad <msiysinyuy@gmail.com>
2021-08-26 17:11:15 +0100

    backend auth updated

--- shopping-card-backend/server.js
+++ shopping-card-backend/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import userRoute from './routes/userRoute'
-
+import productRoute from './routes/productRoute'
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL
@@ -18,21 +18,22 @@ mongoose.connect(mongodbUrl,{
 
 const app = express();
 app.use(bodyParser.json());
-app.get('/api/products',(req,res)=>{
-  res.send(data.products)
-})
+// app.get('/api/products',(req,res)=>{
+//   res.send(data.products)
+// })
 
 app.use('/api/users', userRoute)
-app.get('/api/products/:id',(req,res)=>{
-  const productId = req.params.id;
-  const product=data.products.find(x=>x.id===Number(productId))
-  if(product){
-    res.send(product)
-  }else{
-    res.status(404).send({msg:"Product Not Found"})
-  }
-
-})
+app.use('/api/products', productRoute)
+// app.get('/api/products/:id',(req,res)=>{
+//   const productId = req.params.id;
+//   const product=data.products.find(x=>x.id===Number(productId))
+//   if(product){
+//     res.send(product)
+//   }else{
+//     res.status(404).send({msg:"Product Not Found"})
+//   }
+//
+// })
 
 app.listen(5000,()=>{
   console.log('Server started at http://localhost:5000')
